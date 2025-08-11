8d9ae2a46b26d5f6966404fae5cda13e31007fb0
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2025-08-11 19:52:48 +0530

    Add files via upload

---
+++ stringpractice/String2.java
@@ -0,0 +1,16 @@
+package com.stringpractice;
+
+public class String2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hello";
+		String res="";
+		for(int i=s.length()-1;i>=0;i--) {
+			res=res+s.charAt(i);
+		}
+		System.out.println(res);
+
+	}
+
+}
